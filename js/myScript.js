b81b8a54cd17d158d0e1252dc5978ff49110c2d4
j8504151750 <j8504151750@gmail.com>
2024-03-14 15:11:40 +0800

    新增中間div

--- js/myScript.js
+++ js/myScript.js
@@ -12,6 +12,17 @@ function openLink(evt, linkName) {
     document.getElementById(linkName).style.display = "block";
     evt.currentTarget.className += " w3-red";
 }
+// 中間彈出的div
+var modalElem = document.getElementById("tyModal");
+        var spanElem = document.getElementsByClassName("tyClose")[0];
+
+        spanElem.onclick = function () {
+            modalElem.style.display = "none";
+        }
+
+        setTimeout(function () {
+            modalElem.style.display = "block";
+        }, 1000);
 
 // Click on the first tablink on load
 document.getElementsByClassName("tablink")[0].click();
